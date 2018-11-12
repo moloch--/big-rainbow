6edd9b09ac7a2e3199cedaabb4fc5918feb3d0a2
moloch <moloch38@gmail.com>
2018-11-12 17:40:25 -0600

    fix hex encoding bug

--- cli/bigrainbow.go
+++ cli/bigrainbow.go
@@ -1,5 +1,20 @@
 package main
 
+/*
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 import (
 	"bufio"
 	"bytes"
@@ -125,6 +140,7 @@ func main() {
 	} else {
 		hashes = flag.Args()
 	}
+
 	hashes = unique(hashes)
 
 	if 0 < len(hashes) {
@@ -141,9 +157,11 @@ func hexToBase64(hashes []string) []string {
 	var base64Hashes []string
 	for _, hash := range hashes {
 		data, err := hex.DecodeString(hash)
-		if err != nil {
+		if err == nil {
 			base64Hash := base64.StdEncoding.EncodeToString(data)
 			base64Hashes = append(base64Hashes, base64Hash)
+		} else {
+			fmt.Printf(WARN+"Hex decode error: %v\n", err)
 		}
 	}
 	return base64Hashes
